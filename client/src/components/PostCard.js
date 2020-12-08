c20b68e3272b68b34438f1d077e0312bd3d8bee3
JSYOvO <wotjq5113@gmail.com>
2020-12-08 23:38:19 +0900

    Single post page

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -5,6 +5,7 @@ import { Card, Icon, Label, Image, Button } from 'semantic-ui-react';
 import moment from 'moment';
 import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
+import DeleteButton from './DeleteButton';
 
 function PostCard({ post: {body, createdAt, id, username, likeCount, commentCount, likes} }) {
     
@@ -48,10 +49,7 @@ function PostCard({ post: {body, createdAt, id, username, likeCount, commentCoun
                         </Label>
                     </Button>
                     {user && user.username === username && (
-                        <Button as="div" color="red" floated="right" onClick={() => console.log("Delete post")}>
-                            <Icon name="trash" style={{margin: 0}}/>
-                        </Button>
-                        
+                        <DeleteButton postId={id} />
                     )}
                 </Card.Content>
             </Card>
