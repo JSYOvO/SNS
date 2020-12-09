848166d6f0b50319bf591fbde4ee0ababf983fda
JSYOvO <wotjq5113@gmail.com>
2020-12-09 23:09:42 +0900

    Adding popups

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,11 +1,12 @@
 import { MongooseDocument } from 'mongoose';
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
-import { Card, Icon, Label, Image, Button } from 'semantic-ui-react';
+import { Card, Icon, Label, Image, Button, Popup } from 'semantic-ui-react';
 import moment from 'moment';
 import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
 import DeleteButton from './DeleteButton';
+import MyPopup from '../util/MyPopup';
 
 function PostCard({ post: {body, createdAt, id, username, likeCount, commentCount, likes} }) {
     
@@ -40,14 +41,17 @@ function PostCard({ post: {body, createdAt, id, username, likeCount, commentCoun
                 <Card.Content extra>
                     <LikeButton user={user} post={{ id, likes, likeCount }}/>
 
-                    <Button as="div" labelPosition="right" as={Link} to={`/posts/${id}`}>
-                        <Button color="blue" basic>
-                            <Icon name="comments" />
+                    <MyPopup content="Comment on post" >
+                        <Button as="div" labelPosition="right" as={Link} to={`/posts/${id}`}>
+                            <Button color="blue" basic>
+                                <Icon name="comments" />
+                            </Button>
+                            <Label basic color="blue" pointing="left">
+                                {commentCount}
+                            </Label>
                         </Button>
-                        <Label basic color="blue" pointing="left">
-                            {commentCount}
-                        </Label>
-                    </Button>
+                    </MyPopup>
+                    
                     {user && user.username === username && (
                         <DeleteButton postId={id} />
                     )}
